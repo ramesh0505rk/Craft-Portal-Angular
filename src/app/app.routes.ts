b59a76a41110c4343587b1be06a66474b2520f8e
RAMESHKUMAR K <ramesh0505rk@gmail.com>
2025-04-21 10:13:52 +0530

    Implemented home, signin & signup components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'signin', component: SigninComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
+
