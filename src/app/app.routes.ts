205972cc21487a4a17301fcb7ba8489639448a20
RAMESHKUMAR K <ramesh0505rk@gmail.com>
2025-04-22 02:44:11 +0530

    Updated Signin component - Completed UI.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
+import { authGuard } from './Services/auth.guard';
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent },
+    { path: 'home', component: HomeComponent, canActivate: [authGuard] },
     { path: 'signin', component: SigninComponent },
     { path: 'signup', component: SignupComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' }
