ccc7350f43b52cfdfb2ac5cf00d2f8499da0f136
RAMESHKUMAR <t476797@deluxe.com>
2025-04-22 16:56:13 +0530

    Updated the signup controller (UI)

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+  constructor(private router: Router) { }
 
+  onSignin() {
+    this.router.navigate(['signin'])
+  }
 }
