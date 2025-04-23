7e8d8787425bdfe6342f79852ce164d97680c02e
RAMESHKUMAR K <ramesh0505rk@gmail.com>
2025-04-23 10:50:52 +0530

    Implemented password visibility toggle

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,18 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.scss'
 })
 export class SigninComponent {
 
+  passwordVisible: boolean = false
+
   constructor(private router: Router) { }
 
   onSignup() {
     this.router.navigate(['signup'])
+  }
+
+  togglePasswordVisibility() {
+    this.passwordVisible = !this.passwordVisible
   }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,17 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+
+  passwordVisible: boolean = false
+
   constructor(private router: Router) { }
 
   onSignin() {
     this.router.navigate(['signin'])
+  }
+
+  togglePasswordVisibility() {
+    this.passwordVisible = !this.passwordVisible
   }
 }
