4d990f75f5eb76401d96a14b8025579e2772e641
RAMESHKUMAR KRISHNASAMY <t476797@deluxe.com>
2025-06-13 20:42:43 +0530

    Updated search container

--- src/app/home/topbar/topbar.component.ts
+++ src/app/home/topbar/topbar.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.scss'
 })
 export class TopbarComponent {
+  isCollapsed = false;
 
+  onCollapseSidebar() {
+    this.isCollapsed = !this.isCollapsed;
+  }
 }
